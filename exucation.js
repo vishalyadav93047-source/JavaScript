c7faaba4df1fe84be3c9783053eb92b610a3a361
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-05-19 23:07:52 +0530

    1st

---
+++ exucation.js
@@ -0,0 +1,145 @@
+// var num1 = 10;
+// var num2 = 20;
+
+// function add(a,b){
+//     a = a+20;
+//     b = b+10;
+//      return a+b;
+// }
+//  var output = add(num1,num2);
+//  var modifiedoutput = output+4;
+
+//  console.log(modifiedoutput);
+
+
+// var num1 = 50;
+// var num2 = 55;
+
+// function add(a,b){
+//     a = a-2;
+//     b = b-1;
+//      return a-b;
+// }
+//  var output = add(num1,num2);
+//  var modifiedoutput = output+4;
+
+//  console.log(modifiedoutput);
+ 
+
+// var a = 100;
+// a = 20;
+// console.log(a);
+
+// function random(){
+//     var a = 200;
+//     console.log(a);
+//     return a;
+
+// }
+// a = random(a);
+// console.log(a);
+
+
+// var a = 100;
+// a = 20;
+// console.log(a);
+
+// function random(){
+//     var a = 200;
+//     console.log(a);
+//     a= a+ 10;
+//     console.log(a);
+//     return a;
+// }
+
+// random(a);
+// console.log(a);
+
+// var a = 100;
+// a = 20;
+// console.log(a);
+
+// function random(){
+//     console.log(a);
+//     var a=10;
+//     console.log(a);
+    
+// }
+
+// random(a);
+// console.log(a);
+
+
+
+// let a = 100;
+// a = 20;
+// console.log(a);
+
+// function random(){
+//     console.log(a);
+//     let a=10;           //  Error Cannot access 'a' before initialization
+//     console.log(a);
+    
+// }
+ 
+// random(a);
+// console.log(a);
+
+
+// var x = 10;
+// function test(){
+//     x=5;
+//     if(x<20){
+//         var x = 50;
+//         console.log(x);  //50       
+        
+//     }else{
+//        console.log("Hello"); 
+//     }
+// }
+// test();
+
+// console.log(x);   //10
+
+
+// var x = 10;
+// {
+//     x=5;
+//     if(x<20){
+//         var x = 50;
+//         console.log(x);
+        
+//     }else{
+//        console.log(x); 
+//     }
+// }
+
+
+// console.log(x);
+
+
+
+let a = 50;
+var b = 40;
+{
+    console.log(b);   //40
+    // console.log(a); //ReferenceError: Cannot access 'a' before initialization
+var b = 90;
+let a = 200;
+function call(){
+    // console.log(b);   //ReferenceError: Cannot access 'b' before initialization
+    console.log(a);  //undefind
+    let b = 55;
+    var a =65;
+    console.log(a);  //65
+    console.log(b);  //55
+}call();
+console.log(a);      //200
+console.log(b);      //90
+}
+console.log(a);      //50
+console.log(b);      //90
+{
+    console.log(a);  //50
+    console.log(b);  //90
+}
