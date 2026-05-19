698111f5d846a8ac9b2c1625dcbf4b2269597b61
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-05-19 23:02:23 +0530

    1st

---
+++ loop.js
@@ -0,0 +1,130 @@
+// for(let i =1900; i<2000; i+=4){
+//     console.log(i)
+// }
+
+// let count = 0;
+//  for(let i =1900; i<2000; i++){
+//     if((i%4 === 0 && i%100 !== 0)||(i % 400 === 0)){
+//         // console.log(i)
+//         count +=1
+       
+//     }
+    
+// }
+//  console.log(count+1)
+
+
+// for(let i =0; i<=100; i++){
+// if(i%2 == 0 )
+//     console.log(i)
+// }
+
+// print 1 to 10 number
+
+// for(i = 1; i<=10; i++){
+//     console.log(i)
+// }
+
+// print odd number between 1 to 20 
+
+// for(i = 1; i<=20; i++){
+//     if(i%2 != 0)
+//     console.log(i)
+// }
+
+
+// print reverse 1 to 10
+
+// for(i = 10; i>=1; i--){
+//     console.log(i)
+// }
+
+
+// print 5 table
+// for(i = 5; i<=50; i++){
+//     if(i%5==0)
+//     console.log(i)
+// }
+
+// for(i = 5; i<=50; i+=5){
+   
+//     console.log(i)        2nd type
+// }
+
+
+// print sum of 1 to 100
+// let  sum = 0;
+// for(let i = 1; i<=100; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+
+// print factorial 5
+// let fact = 1;
+// for(i =5; i>=1; i--){
+//     fact*=i
+// }
+// console.log(fact)
+
+// print star 
+// for(let i = 1; i<=5; i++){
+//     let star ="";
+
+//     for(let j =1; j<=i; j++){
+//         star = star + "*";
+//     }
+//     console.log(star);
+// }
+
+
+
+
+// print reverse star
+// for(let i = 5; i >= 1; i--) {
+//     let star = "";
+
+//     for(let j = 1; j <= i; j++) {
+//         star = star + "*";
+//     }
+
+//     console.log(star);
+// }
+
+
+// for(let i = 1; i <= 5; i++) {
+//     let star = "";
+
+//     for(let j = 1; j <=5-i; j++) {
+//         star +=" ";
+//     }
+
+//     for(let k= 1; k<=(2*i-1); k++){
+//         star +="*";
+
+//     }
+
+//     console.log( star);
+// }
+
+// for(let i = 5; i >= 1; i--) {
+//     let star = "";
+
+//     for(let j = 1; j <=5-i; j++) {
+//         star +=" ";
+//     }
+
+//     for(let k= 1; k<=(2*i-1); k++){
+//         star +="*";
+
+//     }
+
+//     console.log( star);
+// }
+
+
+
+//While loop
+
+
+
