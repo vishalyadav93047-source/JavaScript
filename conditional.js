6b51430887183f39c69f0834216837ffdfc5ca6e
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-05-19 23:06:52 +0530

    1st

---
+++ conditional.js
@@ -0,0 +1,27 @@
+     let age = 20;
+    //  if (age>=18  && age<=100){
+    //      console.log("can vote");
+    //  }
+    //  else{
+    //     console.log("can't vote")
+    //  }
+         
+
+    //  const result = age>=18 && age<100? "can vote" : "can't vote"
+    //  console.log(result)
+
+
+    let marks = 20;
+    
+if(marks>=60){
+    console.log("first")
+}else if (marks>=50 && marks<60){
+    console.log("second")
+}else if(marks>=30 && marks<50){
+    console.log("third")
+}else{
+    console.log("fail")
+}
+ 
+
+    
