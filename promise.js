d99e04ca5b7d782036a5aa167d9871179442249d
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-05-25 22:50:07 +0530

    1st

--- promise.js
+++ promise.js
@@ -9,7 +9,15 @@
 // console.log(mydata);
 
 
-
+// const mydata = new Promise ((resolve, reject)=>{
+//     let user = true;
+    
+//     setTimeout(()=>{
+//         console.log("This is data.........");
+        
+//     },2000);
+// })
+// console.log(mydata);
 
 
 
