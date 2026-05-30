ee251793019a3a288f677aa9e970f7dfd3e2b8d8
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-05-30 22:23:15 +0530

    1st

--- loop.js
+++ loop.js
@@ -128,3 +128,23 @@
 
 
 
+
+
+
+// for(let i = 5; i >= 1; i--) {
+//     let star = "";
+
+//     for(let j = 1; j <=5-i; j++) {
+//         star +=" ";
+//     }
+
+//     for(let k= 1; k<=(2*i-1); k++){
+//         star +="*";
+
+//     }
+
+//     console.log( star);
+// }
+
+
+
