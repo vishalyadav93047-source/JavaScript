5ba74d77eee53e99de73881ecfe2a1ab7b9471c7
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-05-30 22:25:53 +0530

    1st

--- variable.js
+++ variable.js
@@ -65,3 +65,23 @@
 
 
 
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// console.log(c);
+
+//  {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
