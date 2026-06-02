401b2a1d502fb17eb3e7f7f586559d9b192bbecd
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-06-02 22:45:02 +0530

    2nd

--- function.js
+++ function.js
@@ -77,23 +77,6 @@
 //     }
 // }
 
-// check(7)
 
-// function countVowels(str) {
-//     let count = 0;
-//     for (let i = 0; i < str.length; i++) {
-//         if (
-//             str[i] == 'a' ||
-//             str[i] =='e' ||
-//             str[i] == 'i' ||
-//             str[i] == 'o' ||
-//             str[i] == 'u'
-//         ) {
-//             count++;
-//         }
-//     }
-//     console.log(count);
-// }
-//  countVowels("education");
 
 
