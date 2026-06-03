64597e142ac54311c3554517142916f89861db3d
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-06-03 23:45:02 +0530

    coomit

--- variable.js
+++ variable.js
@@ -45,23 +45,25 @@
 
 // console.log(name);
 
-var a = 10;
-let b = 20;
-const c = 30;
+// var a = 10;
+// let b = 20;
+// const c = 30;
 
-console.log(c);
+// console.log(c);
 
- {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
+//  {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
 
 
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
 
 
 
