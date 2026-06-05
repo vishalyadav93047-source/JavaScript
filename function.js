0f3a614a5ccf6dcae0fbeb6589e17e72323c7c8a
vishalyadav93047 <vishalyadav93047@gmail.com>
2026-06-05 23:22:46 +0530

    1st

--- function.js
+++ function.js
@@ -79,21 +79,21 @@
 
 // check(7)
 
-// function countVowels(str) {
-//     let count = 0;
-//     for (let i = 0; i < str.length; i++) {
-//         if (
-//             str[i] == 'a' ||
-//             str[i] =='e' ||
-//             str[i] == 'i' ||
-//             str[i] == 'o' ||
-//             str[i] == 'u'
-//         ) {
-//             count++;
-//         }
-//     }
-//     console.log(count);
-// }
-//  countVowels("education");
+function countVowels(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (
+            str[i] == 'a' ||
+            str[i] =='e' ||
+            str[i] == 'i' ||
+            str[i] == 'o' ||
+            str[i] == 'u'
+        ) {
+            count++;
+        }
+    }
+    console.log(count);
+}
+ countVowels("education");
 
 
